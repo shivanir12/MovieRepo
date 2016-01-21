3fc9c5137633b5e3a3f00139f06d07e326a1e149
Renjith G R <grenjithr@gmail.com>
2016-01-22 02:06:47 +0530

    Seed data added

--- src/main/java/com/shivani/citra/Application.java
+++ src/main/java/com/shivani/citra/Application.java
@@ -2,30 +2,40 @@ package com.shivani.citra;
 
 import com.shivani.citra.model.Movie;
 import com.shivani.citra.repository.MovieRepository;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-//import java.util.Arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         MovieRepository movieRepository = ctx.getBean(MovieRepository.class);
 
-        movieRepository.save(new Movie("Spectre"));
+        File seedDirectory = new File(ClassLoader.getSystemClassLoader().getResource("seed").toURI());
 
-        Stream.of(movieRepository.findAll()).forEach(movie -> System.out.println(movie.toString()));
+        Arrays.stream(seedDirectory.listFiles()).forEach(file -> {
+            try {
+                JSONObject jsonObject = new JSONObject(new JSONTokener(new FileReader(file)));
+                movieRepository.save(new Movie(jsonObject.getString("title")));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
-//        String[] beanNames = ctx.getBeanDefinitionNames();
-//        Arrays.sort(beanNames);
-//        for (String beanName : beanNames) {
-//            System.out.println(beanName);
-//        }
+        Stream.of(movieRepository.findAll()).forEach(movies -> System.out.println(movies.toString()));
+
     }
 
 }
