059995ee3cf1f53f60e6e88f371103dd07a1094a
Shivani R <shivanir@thoughtworks.com>
2016-01-23 16:32:17 +0530

    Change movies endpoint to return content type json

--- src/main/java/com/shivani/citra/controller/MovieController.java
+++ src/main/java/com/shivani/citra/controller/MovieController.java
@@ -4,6 +4,7 @@ package com.shivani.citra.controller;
 import com.shivani.citra.model.Movie;
 import com.shivani.citra.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @RequestMapping(value = "/movies", method = RequestMethod.GET)
+    @RequestMapping(value = "/movies", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Movie> getMovie() {
         return movieService.getAllMovies();
     }
