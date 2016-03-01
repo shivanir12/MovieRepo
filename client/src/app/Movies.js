273d004c4922e4c2735356c47b9480851204b08b
Shivani R <shivanir@thoughtworks.com>
2016-03-01 18:15:23 +0530

    Created a provider to call backend apis

--- client/src/app/Movies.js
+++ client/src/app/Movies.js
@@ -1,19 +1,38 @@
 var obj = angular.module("app", ['ngRoute']);
 
-    obj.config(function($httpProvider) {
-          $httpProvider.defaults.useXDomain = true;
-          delete $httpProvider.defaults.headers.common['X-Requested-With'];
-      });
-
-    obj.controller("movieDetails", ["$scope", "$http", function($scope, $http) {
+    obj.controller("movieController", ["$scope", "movieDetails", function($scope, movieDetails) {
     $scope.Name = "shivani";
         $scope.displayMovies = function() {
-          $http({
-            url : 'http://localhost:8080/api/movies',
-            method: 'GET',
-          }).then(function(resp) {
-                $scope.response = resp.data;
-          }, function(resp) {
-          });
+          $scope.response = movieDetails.getMovies();
         };
     }]);
+
+    obj.provider("movieDetails", function() {
+        var baseUrl = '';
+        this.config = function(url){
+            baseUrl = url;
+        };
+
+        this.$get = ["$http", "$log", function($http, $log) {
+        $log.log("instantiating provider");
+        var providerObj = {};
+        providerObj.getMovies = function() {
+            $http({
+                   url : 'localhost:8080/api/movies',
+                   method: 'GET',
+                   }).then(function(resp) {
+                       return resp.data;
+                   }, function(resp) {
+                        $log.log("Error occured");
+                   });
+        };
+        return providerObj;
+        }];
+    });
+
+    obj.config("movieDetailsProvider", function($httpProvider, movieDetailsProvider) {
+              movieDetailsProvider.config("localhost:8080/api");
+              $httpProvider.defaults.useXDomain = true;
+              delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    });
+
